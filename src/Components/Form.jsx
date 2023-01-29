e5655d09ddf31e28544a928c6cde7014073dc351
shandie <shancarl.belgica.3@gmail.com>
2023-01-29 09:18:15 +0800

    added file upload config

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,37 +1,66 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../index.css';
+import { toast } from 'react-toastify';
+import Loading from '../Shared/Loading';
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+
 function Form() {
+  const [loading, setLoading] = useState(false);
   const [projects, setProjects] = useState({
     name: '',
-    img: '',
+    projectImage: '',
     live: '',
     repo: '',
     desc: '',
+    category: '',
   });
-  const { name, img, live, repo, desc } = projects;
+  const { name, projectImage, live, repo, desc, category } = projects;
 
   const onMutate = (e) => {
-    setProjects((prevSate) => ({
-      ...prevSate,
-      [e.target.id]: e.target.value,
-    }));
+    if (e.target.file) {
+      setProjects((prevSate) => ({
+        ...prevSate,
+        projectImage: e.target.file,
+      }));
+    }
+    if (!e.target.file) {
+      setProjects((prevSate) => ({
+        ...prevSate,
+        [e.target.id]: e.target.value,
+      }));
+    }
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
+    setLoading(true);
+    console.log(projects);
 
-    axios.post(`https://groovy-groove-268003.firebaseio.com/projects.json`, {
-      name: projects.name,
-      img: projects.img,
-      live: projects.live,
-      repo: projects.repo,
-      desc: projects.desc,
-    });
-    setProjects('');
-    alert('Submitted Succesfully');
+    axios
+      .post(`${API_ENDPOINT}/projects`, {
+        name: projects.name,
+        projectImage: projects.projectImage,
+        desc: projects.desc,
+        live: projects.live,
+        repo: projects.repo,
+        category: projects.category,
+      })
+      .then((res) => {
+        console.log(res);
+        setProjects('');
+        toast.success('Submitted Succesfully');
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        toast.error('Project upload failed. Try again.');
+      });
   };
-  return (
+  return loading ? (
+    <Loading />
+  ) : (
     <>
       <form onSubmit={submitHandler}>
         <label htmlFor='name'>Name</label>
@@ -47,10 +76,12 @@ function Form() {
         <label htmlFor='img'>Img url</label>
         <br />
         <input
-          type='url'
-          id='img'
-          value={img}
+          type='file'
+          id='projectImage'
           className='form'
+          accept='.png, .jpg, .jpeg'
+          max={1}
+          required
           onChange={onMutate}
         />
         <br />
@@ -73,6 +104,15 @@ function Form() {
           className='form'
           onChange={onMutate}
         />
+        <br />
+        <label htmlFor='repo'>Category</label>
+        <br />
+        <select name='' id='category' value={category} onChange={onMutate}>
+          <option value='fullstack'>Full-stack</option>
+          <option value='frontend'>Front-End</option>
+          <option value='api'>API</option>
+          <option value='app'>Native App</option>
+        </select>
         <br />
         <label htmlFor='repo'>Description</label>
         <br />
