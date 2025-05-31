4ab072c50063cf6f25f462a21cc1804bc3ed2667
chancharl <48678135+dickheadph@users.noreply.github.com>
2025-05-31 19:46:23 +0800

    Update Socials.jsx
    Update socials

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -6,10 +6,10 @@ function Socials() {
     <>
       <div className='absolute left-12 z-10 hidden lg:bottom-[20%] lg:block lg:text-sm xl:bottom-[10%] xl:text-base'>
         <ul className='flex text-zinc-700 lg:block lg:space-y-3'>
-          <a href='https://github.com/shanegel'>
+          <a href='https://github.com/dickheadph'>
             <li className='flex items-center'>
               <SiGithub className='animate-bounce' />
-              &nbsp;https://github.com/shanegel
+              &nbsp;https://github.com/dickheadph
             </li>
           </a>
           <li className='flex items-center'>
@@ -20,9 +20,12 @@ function Socials() {
             <FaMobile className={''} />
             &nbsp;+63 915-203-0863
           </li>
+          <a href='www.linkedin.com/in/shabelgica-ex0718'>
           <li>
             <SiLinkedin className={''} />
+            &nbsp;www.linkedin.com/in/shabelgica
           </li>
+          </a>
           <li>
             <SiIndeed className={''} />
           </li>
