2d68352a4f13c9b772d2e8cc8f0bbafaaa58024b
chancharl <48678135+dickheadph@users.noreply.github.com>
2025-05-31 20:19:09 +0800

    Update Socials.jsx
    Update order

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -6,20 +6,20 @@ function Socials() {
     <>
       <div className='absolute left-12 z-10 hidden lg:bottom-[20%] lg:block lg:text-sm xl:bottom-[10%] xl:text-base'>
         <ul className='flex text-zinc-700 lg:block lg:space-y-3'>
-          <a href='https://github.com/dickheadph'>
-            <li className='flex items-center'>
-              <SiGithub className='animate-bounce' />
-              &nbsp;https://github.com/dickheadph
-            </li>
-          </a>
-          <li className='flex items-center'>
-            <FaEnvelope className={''} />
-            &nbsp;shancarl.belgica.3@gmail.com
-          </li>
           <li className='flex items-center'>
             <FaMobile className={''} />
             &nbsp;+63 915-203-0863
           </li>
+          <li className='flex items-center'>
+            <FaEnvelope className={''} />
+            &nbsp;shancarl.belgica.3@gmail.com
+          </li>
+          <a href='https://github.com/dickheadph'>
+            <li className='flex items-center'>
+              <SiGithub className='' />
+              &nbsp;https://github.com/dickheadph
+            </li>
+          </a>
           <a href='https://www.linkedin.com/in/shabelgica-ex0718'>
             <li className='flex items-center'>
               <SiLinkedin className={''} />
