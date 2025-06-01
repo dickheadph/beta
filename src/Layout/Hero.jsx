d472410b11745424556d1042254dba500eac2eb4
chancharl <48678135+dickheadph@users.noreply.github.com>
2025-06-01 14:34:04 +0800

    Update Hero.jsx
    Update img

--- src/Layout/Hero.jsx
+++ src/Layout/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Wave from '../Components/Wave';
-import Me from '../assets/src/assets/me.png';
+import Me from '../assets/src/me.png';
 import Navbar from './Navbar';
 function Hero() {
   return (
