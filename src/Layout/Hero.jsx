02a5353ddcd9453130268f6f4342867a59ef34a4
chancharl <48678135+dickheadph@users.noreply.github.com>
2025-06-01 14:36:25 +0800

    Update Hero.jsx
    Add img path

--- src/Layout/Hero.jsx
+++ src/Layout/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Wave from '../Components/Wave';
-import Me from '../assets/src/me.png';
+import Me from '../src/assets/me.png';
 import Navbar from './Navbar';
 function Hero() {
   return (
