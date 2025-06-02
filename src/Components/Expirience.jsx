c7183153c5d3ef8b37ce4a5265d1296ee2c94ca6
dickheadph <shancarl.belgica.3@gmail.com>
2025-06-02 14:36:27 +0800

    Add company

--- src/Components/Expirience.jsx
+++ src/Components/Expirience.jsx
@@ -6,11 +6,11 @@ export const Expirience = () => {
     <h1 className='my-[4%] text-center text-3xl font-bold md:text-zinc-800 xl:text-4xl'>
     EXPERIENCE
   </h1>
-  <div className='lg:flex flex-flex-wrap text-xs lg:text-base'>
+  <div className='lg:flex flex-flex-wrap items-between text-xs lg:text-base'>
     <div className='text-left lg:m-6'>
-    <h2 className='text-xl font-semibold text-zinc-800 xl:text-black px-[10%] lg:px-[15%]'>Associate, <em className='text-[#f0b547]'> Application</em> Management
-    {/*<h3 className='my-2'>2023 - Present</h3>
-    <p className='text-sm lg:text-sm xl:text-base'>As an Associate in Application Management wherein I oversee the support, optimization, and administration of SaaS applications using REST API & MsSQL. I am a highly motivated tech support, web designer and a tech savvy individual hoping to hone, leverage and widen my horizon in IT Industry and related technologies. To apply the knowledge and skills I learned in my profession, be part of a strong workforce and contribute positive results to an organization.</p>*/}
+    <h2 className='text-xl font-semibold text-zinc-800 xl:text-black'>Associate, <em className='text-[#f0b547]'> Application</em> Management
+    <h3 className='my-2 text-base'>STRADA(Alight Solutions)</h3>
+    {/*<p className='text-sm lg:text-sm xl:text-base'>As an Associate in Application Management wherein I oversee the support, optimization, and administration of SaaS applications using REST API & MsSQL. I am a highly motivated tech support, web designer and a tech savvy individual hoping to hone, leverage and widen my horizon in IT Industry and related technologies. To apply the knowledge and skills I learned in my profession, be part of a strong workforce and contribute positive results to an organization.</p>*/}
     <p className='text-sm lg:text-sm xl:text-base'>2023 - Present</p>
     </h2>
     </div>
